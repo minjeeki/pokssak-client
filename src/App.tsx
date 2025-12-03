f07b27c56c7cd39e90f6ec5a575027c1ec585780
hjunieee <hinara12@inu.ac.kr>
2025-12-03 23:57:26 +0900

    feat: 라우팅 구조 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import PageLayout from "./layouts/PageLayout";
+import HomePage from "./pages/HomePage";
+import MyPage from "./pages/MyPage";
+import PlaceDetailPage from "./pages/PlaceDetailPage";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PageLayout />}>
+          <Route path="home" element={<HomePage />} />
+          <Route path="detail/:id" element={<PlaceDetailPage />} />
+          <Route path="mypage" element={<MyPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/PlaceDetailPage.tsx
@@ -0,0 +1,7 @@
+import { Flex } from "@vapor-ui/core";
+
+const PlaceDetailPage = () => {
+  return <Flex>장소 디테일 페이지입니다.</Flex>;
+};
+
+export default PlaceDetailPage;
