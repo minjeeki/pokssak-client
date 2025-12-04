0ac708a7794f3aa0387262e361f03520ad7d3adf
hjunieee <hinara12@inu.ac.kr>
2025-12-04 13:21:17 +0900

    Feat: 카카오맵 연결

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,40 +12,53 @@ const HomePage = () => {
   const [level, setLevel] = useState<number>(3);
 
   useEffect(() => {
-    if (window.kakao && mapContainer.current) {
-      const options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        level: 4,
-      };
-
-      setTimeout(() => {
-        const kakaoMap = new window.kakao.maps.Map(mapContainer.current, options);
-        setMap(kakaoMap);
-      }, 200);
+    if (!window.kakao) {
+      return;
     }
+
+    const container = mapContainer.current;
+    if (!container) {
+      return;
+    }
+
+    const options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 4,
+    };
+
+    const timer = setTimeout(() => {
+      const kakaoMap = new window.kakao.maps.Map(container, options);
+      setMap(kakaoMap);
+    }, 200);
     console.log(level);
-  }, [mapContainer]);
+
+    return () => clearTimeout(timer);
+  }, []); // dependency는 빈 배열
 
   return (
     <div id="map" className="map_wrap" ref={mapContainer}>
       {/* 지도타입 컨트롤 div */}
       <div className="custom_typecontrol radius_border">
-        <span id="btnRoadmap" className="selected_btn" onClick={() => setMapType(map, "roadmap")}>
+        <span
+          id="btnRoadmap"
+          className="selected_btn"
+          onClick={() => map && setMapType(map, "roadmap")}
+        >
           지도
         </span>
-        <span id="btnSkyview" className="btn" onClick={() => setMapType(map, "skyview")}>
+        <span id="btnSkyview" className="btn" onClick={() => map && setMapType(map, "skyview")}>
           스카이뷰
         </span>
       </div>
       {/* 지도 확대, 축소 컨트롤 div */}
       <div className="custom_zoomcontrol radius_border">
-        <span onClick={() => zoomIn(map, setLevel)}>
+        <span onClick={() => map && zoomIn(map, setLevel)}>
           <img
             src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png"
             alt="확대"
           />
         </span>
-        <span onClick={() => zoomOut(map, setLevel)}>
+        <span onClick={() => map && zoomOut(map, setLevel)}>
           <img
             src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png"
             alt="축소"
